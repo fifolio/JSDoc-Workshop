cf72437e750ebc1c087d3383dfd978faf0390da7
Firas <firasdabbabi@gmail.com>
2023-03-07 11:58:14 +0100

    version 1.4: Array

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 // @ts-check
 
 /**
- *
- *
  * Desc: This is the Pen Name.
- * @type {string}
+ * @type {String}
  */
 
 const petName = "Ruby";
 
 /**
  * Desc: This is the number of pets
- * @type {number}
+ * @type {Number}
  */
 const petNumber = 100;
+
+/**
+ * Desc: This is an array includes details with different types,
+ * @type {Array}
+ */
+const myPetsPrices = [138.4, 522.3, 42.5, 43.23, 99, 75, "hi", true];
+
+/**
+ * Desc: This Array includes the available Pets names
+ * @type {Array<String>}
+ */
+const myPetsNames = ["Alissa", "Vies", "Roda", "Mea"];
